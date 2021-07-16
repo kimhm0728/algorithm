10106cb8f0490ce665018ceb211f971fe5f0e350
kimhm0728 <kimhm0728@naver.com>
2021-07-16 21:20:52 +0900

    1010번 수정

--- baekjoon/src/baekjoon/B_1010_2.java
+++ baekjoon/src/baekjoon/B_1010_2.java
@@ -23,10 +23,11 @@ public class B_1010_2 {
 	static int combi(int a, int b) { // aCb
 		if(dp[a][b] > 0) // 값이 이미 계산된 경우 그 값을 반환
 			return dp[a][b]; 
-		else if (a == b || b == 0)
-			return 1;
-		else 
-			return combi(a - 1, b - 1) + combi(a - 1, b);
+		
+		if (a == b || b == 0)
+			return dp[a][b] = 1;
+		
+		return dp[a][b] = combi(a - 1, b - 1) + combi(a - 1, b);
 		// 조합의 성질을 이용하지 않고 팩토리얼로 풀면 값의 범위가 너무 커 BigInteger 클래스나 double 자료형을 사용해야 함.
 	} 
 } 
