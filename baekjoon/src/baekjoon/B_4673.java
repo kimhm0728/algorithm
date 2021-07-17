3cd40ee0fb3a206f6fefa0137f08f898b0c935c2
kimhm0728 <kimhm0728@naver.com>
2021-07-17 22:27:56 +0900

    백준 4673번 풀이

--- baekjoon/src/baekjoon/B_4673.java
+++ baekjoon/src/baekjoon/B_4673.java
@@ -2,21 +2,21 @@ package baekjoon;
 
 public class B_4673 {
 	static int[] num=new int[10036];
-	// 0~10000 중 9999를 생성자로 가지는 10035이 가장 큰 수이므로
+	// 0~10000 중 9999를 생성자로 가지는 10035이 가장 큰 수이므로 
 	public static void main(String[] args) {
 		for(int i=1;i<=10000;i++)
 			num[selfNumber(i)]++;
-		// 생성자의 갯수만큼 배열의 값을 가짐. 즉 셀프 넘버는 배열의 값이 0
+		// 생성자의 갯수만큼 배열의 값을 가짐. 즉 셀프 넘버는 값이 0
 		for(int i=1;i<=10000;i++)
 			if(num[i] == 0)
 				System.out.println(i);
 	}
 	static int selfNumber(int n) {
-		int a = n;
+		int result = n;
 		for(int i=4;i>=0;i--) {
-			a += n / Math.pow(10, i);
+			result += n / Math.pow(10, i);
 			n %= Math.pow(10, i);
 		}
-		return a;
+		return result;
 	}
 }
