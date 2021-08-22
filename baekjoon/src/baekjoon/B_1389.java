7918e65c73c26a6ca785203fbddb8676a96d47a7
kimhm0728 <kimhm0728@naver.com>
2021-08-22 23:59:23 +0900

    21-08-22

--- baekjoon/src/baekjoon/B_1389.java
+++ baekjoon/src/baekjoon/B_1389.java
@@ -1,68 +1,65 @@
 package baekjoon;
-import java.util.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class B_1389 {
-	static int[] user; // 유저 별 케빈 베이컨 수를 저장
-	static int[][] rel; // 친구 관계를 저장
-	static int[][] visited; // 모든 케빈 베이컨 수 저장
 	static int N, M;
+	static ArrayList<Integer>[] rel;
+	static int[] user; // 유저 별 케빈 베이컨 수
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		user = new int[N];
-		rel = new int[N][N];
-		visited = new int[N][N];
-		for(int i=0;i<M;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken()); 
-			rel[a-1][b-1] = 1; // 유저 번호는 1~N이고 배열의 인덱스는 0~N-1이므로
-			rel[b-1][a-1] = 1;
-		}
-		for(int i=0;i<N;i++)
-			for(int j=0;j<N;i++) {
-				if(i != j)
-					visited[i][j] = kebinBacon(i, j);
-			}
-		int min = 0;
-		for(int i=1;i<N;i++) // 케빈 베이컨이 가장 작은 유저를 찾음
-			if(user[min] > user[i])
-				min = i;
-		for(int i=0;i<N;i++) // 케빈 베이컨 수가 같으면 유저 번호가 작은 유저를 출력
-			if(min != i && visited[min] == visited[i] && min > i)
-				min = i;
-		System.out.println(min);
-	}
-	static int kebinBacon(int n, int m) {
-		int tmp = n;
-		for(int i=0;i<N;i++) {
-			if(rel[n][i] == 1)
-				tmp = i;
-		}
-		if(rel[n][m] == 1)
-			return rel[n][m] = 1;
-		else 
-			return rel[n][m] = 1 + kebinBacon(tmp, n); //
-
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.offer(n);
-		visited[]
 		
-		if(visited[n][m] != 0)
-			return visited[n][m];
-		int temp = n;
-		while(rel[temp][m] != 1) {
-			temp = 
-			if(rel[n][m] == 1) {
-				visited[n][m] 
-				return;
-			}
-			if(visited[temp, m] == 0)
-				visited[temp] = visited[]
+		N = Integer.parseInt(st.nextToken()); // 유저 수
+		M = Integer.parseInt(st.nextToken()); // 친구 관계 수
+		
+		rel = new ArrayList[N];
+		user = new int[N];
+		
+		for(int i=0;i<N;i++)
+			rel[i] = new ArrayList<Integer>();
+		
+		for(int i=0;i<M;i++) { // 친구 관계 입력
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			rel[a].add(b);
+			rel[b].add(a);
 		}
-	} 
+		
+		for(int i=0;i<N;i++) 
+			// 유저 i의 총 케빈 베이컨 수 구하기
+			user[i] = BFS(i); 
+		
+		int min = 0;
+		for(int i=1;i<N;i++) {
+			if(user[i] < user[min])
+				min = i;
+			else if(user[i] == user[min] && min > i)
+				min = i;
+		}
+		System.out.println(min + 1);
+	}
+	static int BFS(int start) {
+		int cnt = 0;
+		int[] kebin = new int[N]; 
+		// kebin[i]는 유저 start에서 유저 i로 가는 단계의 수
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+		while(!q.isEmpty()) {
+			int tmp = q. poll();
+			for(int i : rel[tmp])
+				if(kebin[i] == 0) {
+					q.add(i);
+					kebin[i] = kebin[tmp] + 1;
+					cnt += kebin[i];
+				}
+		}
+		return cnt;
+	}
 }
