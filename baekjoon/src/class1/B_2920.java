583c162896d524470b786801fda8ec069b9181de
kimhm0728 <kimhm0728@naver.com>
2022-01-03 06:40:29 +0900

    22-01-02

---
+++ baekjoon/src/class1/B_2920.java
@@ -0,0 +1,18 @@
+package class1;
+import java.io.*;
+
+public class B_2920 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String scale = br.readLine();
+		scale = scale.replaceAll(" ", "");
+		
+		if(scale.equals("12345678"))
+			System.out.println("ascending");
+		else if(scale.equals("87654321"))
+			System.out.println("descending");
+		else
+			System.out.println("mixed");
+	}
+
+}
