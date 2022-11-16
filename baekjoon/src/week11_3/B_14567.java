9fb4e0468df4a7ad714f3a9ef3e2be2a99d34ef6
kimhm0728 <kimhm0728@naver.com>
2022-11-17 04:03:25 +0900

    add files

--- baekjoon/src/week11_3/B_14567.java
+++ baekjoon/src/week11_3/B_14567.java
@@ -12,7 +12,7 @@ public class B_14567 {
 		int N = Integer.parseInt(st.nextToken()); // 과목 수
 		int M = Integer.parseInt(st.nextToken()); // 선수 조건 수
 
-		StringBuilder sb = new StringBuilder("0 ".repeat(N));
+		StringBuilder sb = new StringBuilder();
 		
 		int indegree[] = new int[N + 1];
 		ArrayList<Integer> list[] = new ArrayList[N + 1];
@@ -31,6 +31,7 @@ public class B_14567 {
 		
 		// 인덱스, 학기
 		Queue<int[]> q = new LinkedList<>();
+		int result[] = new int[N];
 		
 		for(int i=1;i<=N;i++)
 			if(indegree[i] == 0) 
@@ -40,12 +41,15 @@ public class B_14567 {
 			int arr[] = q.poll();
 			int idx = arr[0];
 			int semester = arr[1];
-			sb.setCharAt((idx - 1) * 2, (char) (semester + '0'));
+			result[idx - 1] = semester;
 			
 			for(int i : list[idx])
 				if(--indegree[i] == 0)
 					q.offer(new int[] {i, semester + 1});
 		}
+		
+		for(int i=0;i<N;i++)
+			sb.append(result[i]).append(' ');
 		
 		System.out.println(sb);
 	}
