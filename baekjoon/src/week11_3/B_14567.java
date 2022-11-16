290f53f2c54d725ac98a798c738e2691575fd888
kimhm0728 <kimhm0728@naver.com>
2022-11-16 21:07:37 +0900

    add files

---
+++ baekjoon/src/week11_3/B_14567.java
@@ -0,0 +1,53 @@
+package week11_3;
+
+import java.util.*;
+import java.io.*;
+
+public class B_14567 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); // 과목 수
+		int M = Integer.parseInt(st.nextToken()); // 선수 조건 수
+
+		StringBuilder sb = new StringBuilder("0 ".repeat(N));
+		
+		int indegree[] = new int[N + 1];
+		ArrayList<Integer> list[] = new ArrayList[N + 1];
+
+		for(int i=1;i<=N;i++)
+			list[i] = new ArrayList<>();
+
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int pre = Integer.parseInt(st.nextToken());
+			int next = Integer.parseInt(st.nextToken());
+
+			list[pre].add(next);
+			indegree[next]++;
+		}
+		
+		// 인덱스, 학기
+		Queue<int[]> q = new LinkedList<>();
+		
+		for(int i=1;i<=N;i++)
+			if(indegree[i] == 0) 
+				q.offer(new int[] {i, 1});
+		
+		while(!q.isEmpty()) {
+			int arr[] = q.poll();
+			int idx = arr[0];
+			int semester = arr[1];
+			sb.setCharAt((idx - 1) * 2, (char) (semester + '0'));
+			
+			for(int i : list[idx])
+				if(--indegree[i] == 0)
+					q.offer(new int[] {i, semester + 1});
+		}
+		
+		System.out.println(sb);
+	}
+
+}
