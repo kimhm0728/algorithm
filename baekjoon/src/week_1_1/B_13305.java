ff866335e2d0b7b7d2c7729fa5fb491cb09b4d17
kimhm0728 <kimhm0728@naver.com>
2023-01-07 22:45:18 +0900

    add files

---
+++ baekjoon/src/week_1_1/B_13305.java
@@ -0,0 +1,34 @@
+package week_1_1;
+
+import java.util.*;
+import java.io.*;
+
+public class B_13305 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer dis = new StringTokenizer(br.readLine());
+		StringTokenizer cos = new StringTokenizer(br.readLine());
+		
+		long[] distance = new long[N - 1];
+		long[] cost = new long[N];
+		
+		for(int i=0;i<N;i++) {
+			cost[i] = Long.parseLong(cos.nextToken());
+			if(i != N - 1)
+				distance[i] = Long.parseLong(dis.nextToken());
+		}
+		
+		long answer = cost[0] * distance[0];
+		long now = cost[0]; // 현재 주유소 가격
+		for(int i=1;i<N-1;i++) {
+			if(cost[i] < now) // 현재보다 더 저렴하면 그 주유소에서 기름을 넣음
+				now = cost[i];
+			answer += distance[i] * now;
+		}
+		
+		System.out.println(answer);
+	}
+
+}
