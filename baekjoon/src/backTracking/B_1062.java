347583b4b7c90b3220e958d9568a007af2a2008f
kimhm0728 <kimhm0728@naver.com>
2023-01-22 15:10:34 +0900

    add files

--- baekjoon/src/backTracking/B_1062.java
+++ baekjoon/src/backTracking/B_1062.java
@@ -4,20 +4,20 @@ import java.util.*;
 import java.io.*;
 
 public class B_1062 {
-	static char[] arr;
+	static String[] str;
 	static boolean[] visit;
-	static Character[] select;
-	static HashSet<Character>[] set;
-	static int N, K, idx;
+	static int N, K;
 	static int answer = 0;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken()); // anta ~ tica
-		
+		K = Integer.parseInt(st.nextToken());
+		str = new String[N];
+		visit = new boolean[26];
+
 		if(K < 5) {
 			System.out.println(0);
 			return;
@@ -26,83 +26,45 @@ public class B_1062 {
 			System.out.println(N);
 			return;
 		}
-		
+
 		K -= 5;
-		set = new HashSet[N];
-		idx = 0; // 실제 값이 존재하는 set
-		
-		int def = 0;
-		HashSet<Character> alpa = new HashSet<>();
-		for(int i=0;i<N;i++) {
-			set[i] = new HashSet<>();
-			
-			String str = br.readLine();
-			str = str.substring(4, str.length() - 4);
-			for(char c : str.toCharArray()) {
-				if(c == 'a' || c == 'n' || c == 't' ||
-						c == 'i' || c == 'c')
-					continue;
-				set[idx].add(c);
-			}
-			
-			int size = set[idx].size();
-			if(size == 0) {
-				def++;
-				set[idx].clear();
-			}
-			else if(size > K) {
-				set[idx].clear();
-			}
-			else {
-				alpa.addAll(set[idx]);
-				idx++;
-			}
-		}
-		
-		int size = alpa.size();
-		arr = new char[size];
-		visit = new boolean[size];
-		Iterator<Character> iter = alpa.iterator();
-		
-		for(int i=0;i<size;i++)
-			arr[i] = iter.next();
-		
-		select = new Character[K];
-		DFS(0, 0, size);
-		System.out.println(answer + def);
-		
+
+		for(int i=0;i<N;i++) 
+			str[i] = br.readLine().replaceAll("anta|tica", "");
+
+		visit['a' - 'a'] = true;
+		visit['n' - 'a'] = true;
+		visit['t' - 'a'] = true;
+		visit['i' - 'a'] = true;
+		visit['c' - 'a'] = true;
+
+		DFS(0, 0);
+		System.out.println(answer);
+
 	}
-	
-	static void DFS(int depth, int start, int n) {
+
+	static void DFS(int depth, int start) {
 		if(depth == K) {
-			answer = Math.max(answer, count());
+			int cnt = 0;
+			Loop : for(int i=0;i<N;i++) {
+				for(int j=0;j<str[i].length();j++)
+					if(!visit[str[i].charAt(j) - 'a'])
+						continue Loop;
+				cnt++;
+			}
+
+			answer = Math.max(answer, cnt);
 			return;
 		}
-		
-		for(int i=start;i<n;i++) {
+
+		for(int i=start;i<26;i++) {
 			if(visit[i])
 				continue;
-			
+
 			visit[i] = true;
-			select[depth] = arr[i];
-			DFS(depth + 1, start + 1, n);
+			DFS(depth + 1, i + 1);
 			visit[i] = false;
 		}
-	}
-	
-	static int count() {
-		ArrayList<Character> list = new ArrayList<>(Arrays.asList(select));
-		
-		int cnt = 0;
-		Loop : for(int i=0;i<idx;i++) {
-			Iterator<Character> iter = set[i].iterator();
-			while(iter.hasNext())
-				if(!list.contains(iter.next()))
-					continue Loop;
-			cnt++;
-		}
-		
-		return cnt;
 	}
 
 }
