9a71c1fdd9be7c2e3221155067eefaa2316cc0e9
kimhm0728 <kimhm0728@naver.com>
2023-03-21 18:06:31 +0900

    add files

---
+++ baekjoon/src/binarySearch/B_2512.java
@@ -0,0 +1,55 @@
+package binarySearch;
+
+import java.util.*;
+import java.io.*;
+
+public class B_2512 {
+	static int N, M;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		M = Integer.parseInt(br.readLine());
+		
+		System.out.println(upperBound());
+	}
+	
+	static int upperBound() {
+		int lo = 0;
+		int hi = arr[N - 1] + 1;
+		
+		while(lo < hi) {
+			int mid = (lo + hi) / 2;
+			if(getSumBudget(mid) <= M)
+				lo = mid + 1;
+			else
+				hi = mid;
+		}
+		
+		return lo - 1;
+	}
+	
+	static int getSumBudget(int budget) {
+		int sum = 0;
+		int i;
+		for(i=0;i<N;i++) {
+			if(budget > arr[i])
+				sum += arr[i];
+			else
+				break;
+		}
+		
+		sum += (N - i) * budget;
+		return sum;
+	}
+
+}
--- baekjoon/src/dynamicProgramming/B_2225.java
+++ baekjoon/src/dynamicProgramming/B_2225.java
@@ -4,29 +4,38 @@ import java.util.*;
 import java.io.*;
 
 public class B_2225 {
+	static final int INF = Integer.MAX_VALUE;
 	static final int div = 1000000000;
-	static int N, K;
 	static int[][] dp;
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-
-		dp = new int[K + 1][N + 1];
 		
-        Arrays.fill(dp[1], 1);
-        for(int i=1;i<=K;i++)
-            dp[i][0] = 1;
-        
-        for(int i=2;i<=K;i++) {
-            for(int j=1;j<=N;j++)
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % div;
-        }
-        
-		System.out.println(dp[K][N]);
+		dp = new int[N + 1][K + 1];
+		
+		for(int i=0;i<=N;i++) {
+			Arrays.fill(dp[i], INF);
+			dp[i][0] = 0;
+		}
+		
+		dp[0][0] = 1;
+		System.out.println(sumCount(N, K));
+	}
+	
+	static int sumCount(int n, int k) {
+		if(dp[n][k] == INF) {
+			int temp = 0;
+			for(int i=n;i>=0;i--) 
+				temp = (temp + sumCount(n - i, k - 1)) % div;
+			
+			dp[n][k] = temp % div;
+		}
+		
+		return dp[n][k];
 	}
     
 }
