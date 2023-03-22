95145d962f3ecf3833674da676f75f5f4d4f1616
kimhm0728 <kimhm0728@naver.com>
2023-03-22 18:44:58 +0900

    add files

---
+++ baekjoon/src/dynamicProgramming/B_22871.java
@@ -0,0 +1,38 @@
+package dynamicProgramming;
+
+import java.util.*;
+import java.io.*;
+
+public class B_22871 {
+	static int N;
+	static long[] a;
+	static long[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		a = new long[N];
+		dp = new long[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) 
+			a[i] = Long.parseLong(st.nextToken());
+
+		System.out.println(isCross(0));
+	}
+
+	static long isCross(int n) {
+		if(n == N - 1)
+			return 0;
+
+		if(dp[n] == 0) {
+			dp[n] = Long.MAX_VALUE;
+			for(int i=n+1;i<N;i++) 
+				dp[n] = Math.min(dp[n], Math.max((i - n) * (Math.abs(a[i] - a[n]) + 1), isCross(i)));
+		}
+		
+		return dp[n];
+	}
+
+}
