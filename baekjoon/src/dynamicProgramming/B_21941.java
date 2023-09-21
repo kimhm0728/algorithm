f55b768c93f9923e93e2a6093f60f5637f591fcc
kimhm0728 <kimhm0728@naver.com>
2023-09-21 16:00:14 +0900

    add files

--- baekjoon/src/dynamicProgramming/B_21941.java
+++ baekjoon/src/dynamicProgramming/B_21941.java
@@ -4,44 +4,29 @@ import java.util.*;
 import java.io.*;
 
 public class B_21941 {
-
+	static String S;
+	static int M;
+	static String[] str;
+	static int[] score;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		int N = Integer.parseInt(br.readLine());
-
-		int len = str.length();
-		int[] dp = new int[len + 1];
-		Delete[] arr = new Delete[N];
+		
+		S = br.readLine();
+		M = Integer.parseInt(br.readLine());
+	
+		str = new String[M];
+		score = new int[M];
 		
 		StringTokenizer st;
-		for(int i=0;i<N;i++) {
+		for (int i=0;i<M;i++) {
 			st = new StringTokenizer(br.readLine());
-			arr[i] = new Delete(st.nextToken(), Integer.parseInt(st.nextToken()));
-		}
-		
-		for(int i=0;i<len;i++) {
-			dp[i + 1] = Math.max(dp[i + 1], dp[i] + 1);
 			
-			for(Delete d : arr) 
-				if(str.startsWith(d.str, i)) 
-					dp[i + d.length] = Math.max(dp[i + d.length], dp[i] + d.score);
+			str[i] = st.nextToken();
+			score[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		System.out.println(dp[len]);
 		
 	}
 	
-	static class Delete {
-		String str;
-		int score;
-		int length;
-		
-		Delete(String str, int score) {
-			this.str = str;
-			this.score = score;
-			length = str.length();
-		}
-	}
-
 }
