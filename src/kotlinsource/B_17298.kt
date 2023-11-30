c6493d3ea05960bd6db75e3699efd725e8291647
kimhm0728 <kimhm0728@naver.com>
2023-11-30 23:53:33 +0900

    add all files

---
+++ src/kotlinsource/B_17298.kt
@@ -0,0 +1,30 @@
+package kotlinsource
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val list = readLine().split(" ").map { it.toInt() }.toMutableList()
+    val nge = mutableListOf<Int>()
+
+    val answer = IntArray(n)
+
+    for (idx in n - 1 downTo 0) {
+        answer[idx] = findNGE(list[idx], nge)
+    }
+
+    print(answer.joinToString(" "))
+}
+
+private fun findNGE(element: Int, nge: MutableList<Int>): Int {
+    while (nge.isNotEmpty()) {
+        val last = nge.last()
+        if (element < last) {
+            nge.add(element)
+            return last
+        }
+
+        nge.removeLast()
+    }
+
+    nge.add(element)
+    return -1
+}
