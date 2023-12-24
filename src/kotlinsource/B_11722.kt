0b13f67d4855b3c968e562a1e9d156214e9d065e
kimhm0728 <kimhm0728@naver.com>
2023-12-24 18:14:55 +0900

    11722

---
+++ src/kotlinsource/B_11722.kt
@@ -0,0 +1,20 @@
+package kotlinsource
+
+import kotlin.math.max
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val arr = readLine().split(" ").map { it.toInt() }.toIntArray()
+    val dp = IntArray(n) { 1 }
+
+    var answer = 1
+    for (i in 1 until n) {
+        for (j in 0 until i) {
+            if (arr[i] >= arr[j]) continue
+            dp[i] = max(dp[i], dp[j] + 1)
+        }
+        answer = max(answer, dp[i])
+    }
+
+    print(answer)
+}
