2bc23db5ddf536e63dd12685ddfa4f0ed9f84c1d
kimhm0728 <kimhm0728@naver.com>
2023-12-24 18:53:23 +0900

    14501

---
+++ src/kotlinsource/B_14501.kt
@@ -0,0 +1,22 @@
+package kotlinsource
+
+import java.util.*
+import kotlin.math.max
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val dp = IntArray(n + 1)
+    var max = 0
+
+    for (idx in 0 until n) {
+        val st = StringTokenizer(readLine())
+        val t = st.nextToken().toInt()
+        val p = st.nextToken().toInt()
+
+        max = max(max, dp[idx])
+        if (idx + t > n) continue
+        dp[idx + t] = max(dp[idx + t], max + p)
+    }
+
+    print(max(max, dp[n]))
+}
