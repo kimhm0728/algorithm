4c1cf6fb4f1d1e69ccf09eca20479ed1118e360e
kimhm0728 <kimhm0728@naver.com>
2024-07-12 22:22:01 +0900

    Update B_2225.kt

--- src/kotlinsource/dp/B_2225.kt
+++ src/kotlinsource/dp/B_2225.kt
@@ -1,33 +1,54 @@
-package kotlinsource.dp
+// package kotlinsource.dp
 
-class B_2225 {
-    private val div = 1_000_000_000
-    private lateinit var dp: Array<IntArray>
+// class B_2225 {
+//     private val div = 1_000_000_000
+//     private lateinit var dp: Array<IntArray>
 
-    operator fun invoke() {
-        val (n, k) = System.`in`.bufferedReader().readLine()
-            .split(" ")
-            .map { it.toInt() }
+//     operator fun invoke() {
+//         val (n, k) = System.`in`.bufferedReader().readLine()
+//             .split(" ")
+//             .map { it.toInt() }
 
-        dp = Array(n + 1) { IntArray(k + 1) }
-        for (idx in 0..n) {
-            dp[idx][1] = 1
-        }
+//         dp = Array(n + 1) { IntArray(k + 1) }
+//         for (idx in 0..n) {
+//             dp[idx][1] = 1
+//         }
 
-        print(solution(n, k))
-    }
+//         print(solution(n, k))
+//     }
 
-    private fun solution(n: Int, k: Int): Int {
-        if (dp[n][k] != 0) return dp[n][k]
+//     private fun solution(n: Int, k: Int): Int {
+//         if (dp[n][k] != 0) return dp[n][k]
 
-        for (i in 0..n) {
-            dp[n][k] = (dp[n][k] + solution(n - i, k - 1)) % div
-        }
+//         for (i in 0..n) {
+//             dp[n][k] = (dp[n][k] + solution(n - i, k - 1)) % div
+//         }
 
-        return dp[n][k]
-    }
-}
+//         return dp[n][k]
+//     }
+// }
+
+// fun main() {
+//     B_2225()()
+// }
+
+val div = 1_000_000_000
+lateinit var dp: Array<IntArray>
 
 fun main() {
-    B_2225()()
+    val (n, k) = System.`in`.bufferedReader().readLine().split(" ").map { it.toInt() }
+    dp = Array(n + 1) { IntArray(k + 1) { 0 } }
+    (0..n).forEach { dp[it][1] = 1 }
+    println(solution(n, k))
+}
+
+fun solution(n: Int, k: Int): Int {
+    if (k == 0) return 0
+    if (dp[n][k] != 0) return dp[n][k]
+    var temp = 0
+    (0..n).forEach {
+        temp = (temp + solution(n - it, k - 1)) % div
+    }
+    dp[n][k] = temp
+    return dp[n][k]
 }
