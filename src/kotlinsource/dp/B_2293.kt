3f7ef6e85714fdc4aca947cf77b0da8633475021
kimhm0728 <kimhm0728@naver.com>
2024-07-17 02:08:17 +0900

    Update B_2293.kt

--- src/kotlinsource/dp/B_2293.kt
+++ src/kotlinsource/dp/B_2293.kt
@@ -1,17 +1,14 @@
-package kotlinsource.dp
-
 fun main() = with(System.`in`.bufferedReader()) {
     val (n, k) = readLine().split(" ").map { it.toInt() }
-    val dp = IntArray(k + 1)
+    val v = IntArray(n) { readLine().toInt() }
+    val dp = IntArray(k + 1) { 0 }
     dp[0] = 1
 
-    for (i in 1..n) {
-        val value = readLine().toInt()
-
-        for (cnt in value..k) {
-            dp[cnt] += dp[cnt - value]
+    v.forEach { value ->
+        (value..k).forEach {
+            dp[it] += dp[it - value]
         }
     }
 
-    print(dp[k])
+    println(dp[k])
 }
