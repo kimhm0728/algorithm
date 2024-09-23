4a50719d929c8d4dcce3fe3165fed881439dd1e8
Hyemin Kim <kimhm0728@naver.com>
2024-09-23 22:59:52 +0900

    [실버2] 11055. 가장 큰 증가하는 부분 수열

--- src/kotlin/dp/B_11055.kt
+++ src/kotlin/dp/B_11055.kt
@@ -1,22 +1,37 @@
-package kotlin.dp
-
 import kotlin.math.max
 
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
     val arr = readLine().split(" ").map { it.toInt() }.toIntArray()
     val dp = IntArray(n) { arr[it] }
-    var answer = arr[0]
-
-    for (i in 1 until n) {
-        var temp = 0
-        for (j in i - 1 downTo 0) {
-            if (arr[i] <= arr[j]) continue
-            temp = max(temp, dp[j])
+    for (i in 1..<n) {
+        for (j in 0..<i) {
+            if (arr[j] >= arr[i]) continue
+            dp[i] = max(dp[i], arr[i] + dp[j])
         }
-        dp[i] += temp
-        answer = max(answer, dp[i])
     }
-
-    print(answer)
+    println(dp.max())
 }
+
+// package kotlin.dp
+
+// import kotlin.math.max
+
+// fun main() = with(System.`in`.bufferedReader()) {
+//     val n = readLine().toInt()
+//     val arr = readLine().split(" ").map { it.toInt() }.toIntArray()
+//     val dp = IntArray(n) { arr[it] }
+//     var answer = arr[0]
+
+//     for (i in 1 until n) {
+//         var temp = 0
+//         for (j in i - 1 downTo 0) {
+//             if (arr[i] <= arr[j]) continue
+//             temp = max(temp, dp[j])
+//         }
+//         dp[i] += temp
+//         answer = max(answer, dp[i])
+//     }
+
+//     print(answer)
+// }
