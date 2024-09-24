b14328aa75ec8f6c101ffd062deea44a948cc1dd
Hyemin Kim <kimhm0728@naver.com>
2024-09-24 20:58:52 +0900

    [실버2] 11053. 가장 긴 증가하는 부분 수열

--- src/kotlin/dp/B_11053.kt
+++ src/kotlin/dp/B_11053.kt
@@ -1,20 +1,35 @@
-package kotlin.dp
-
 import kotlin.math.max
 
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
     val arr = readLine().split(" ").map { it.toInt() }.toIntArray()
     val dp = IntArray(n) { 1 }
-    var answer = 1
-
-    for (i in 1 until n) {
-        for (j in 0 until i) {
-            if (arr[i] <= arr[j]) continue
+    for (i in 1..<n) {
+        for (j in 0..<i) {
+            if (arr[j] >= arr[i]) continue
             dp[i] = max(dp[i], dp[j] + 1)
         }
-        answer = max(dp[i], answer)
     }
-
-    print(answer)
+    println(dp.max())
 }
+
+// package kotlin.dp
+
+// import kotlin.math.max
+
+// fun main() = with(System.`in`.bufferedReader()) {
+//     val n = readLine().toInt()
+//     val arr = readLine().split(" ").map { it.toInt() }.toIntArray()
+//     val dp = IntArray(n) { 1 }
+//     var answer = 1
+
+//     for (i in 1 until n) {
+//         for (j in 0 until i) {
+//             if (arr[i] <= arr[j]) continue
+//             dp[i] = max(dp[i], dp[j] + 1)
+//         }
+//         answer = max(dp[i], answer)
+//     }
+
+//     print(answer)
+// }
