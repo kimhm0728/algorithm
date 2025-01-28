9c83000036402fe103ffd55cd0e17dfd68f40ddb
Hyemin Kim <kimhm0728@naver.com>
2025-01-28 17:39:04 +0900

    [골드4] 1744. 수 묶기

--- src/kotlin/greedy/B_1744.kt
+++ src/kotlin/greedy/B_1744.kt
@@ -1,57 +1,33 @@
 fun main() = with(System.`in`.bufferedReader()) {
-    val max = 1001
     val n = readLine().toInt()
     val arr = IntArray(n) { readLine().toInt() }.sorted()
+    val visit = BooleanArray(n)
 
-    var zero = 0
-    var negative = 0
-    for (i in arr) {
-        if (i == 0) {
-            zero++
-        } else if (i < 0) {
-            negative++
-        }
-    }
     var answer = 0
-    if (negative > 0) {
-        var count = 0
-        var temp = 0
-        val block: () -> Boolean = if (zero > 0 && negative % 2 == 1) {
-            { count + 1 == negative }
-        } else {
-            { count == negative }
+    for (i in 0..<n - 1 step (2)) {
+        if (arr[i] <= 0 && arr[i + 1] <= 0) {
+            answer += arr[i] * arr[i + 1]
+            visit[i] = true
+            visit[i + 1] = true
+            continue
         }
-        for (i in 0..<n) {
-            if (arr[i] < 0) {
-                count++
-                if (temp == 0) {
-                    temp = arr[i]
-                } else {
-                    answer += temp * arr[i]
-                    temp = 0
-                }
-                if (block()) {
-                    break
-                }
-            }
-        }
-        if (temp != 0 && !(zero > 0 && negative % 2 == 1)) answer += temp
+        break
     }
-    var temp = max
-    for (i in n - 1 downTo 0) {
-        if (arr[i] <= 0) {
-            break
+
+    for (i in n - 1 downTo 1 step (2)) {
+        if (arr[i] > 1 && arr[i - 1] > 1) {
+            answer += arr[i] * arr[i - 1]
+            visit[i] = true
+            visit[i - 1] = true
+            continue
         }
-        if (temp == max) {
-            temp = arr[i]
-        } else {
-            if (temp == 1 || arr[i] == 1 || arr[i] == 0) {
-                answer += temp + arr[i]
-            } else {
-                answer += temp * arr[i]
-            }
-            temp = max
+        break
+    }
+
+    for (i in 0..<n) {
+        if (!visit[i]) {
+            answer += arr[i]
         }
     }
-    print(if (temp == max) answer else answer + temp)
+    print(answer)
 }
